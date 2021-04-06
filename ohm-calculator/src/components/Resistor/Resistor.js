0c471f0bfe85d051f596f3f1a4ae9a8976a270ae
Arturo Rodriguez <arturo.rdz3000@gmail.com>
2021-04-05 23:02:43 -0500

    Adding configuration for the resistor component

--- ohm-calculator/src/components/Resistor/Resistor.js
+++ ohm-calculator/src/components/Resistor/Resistor.js
@@ -1,10 +1,21 @@
 import './Resistor.css';
 
-function Resistor() {
+function Resistor(props) {
+    const bandsConfiguration = props.bandsConfiguration;
+    const bands = [];
+
+    for (const [index, value] of bandsConfiguration.entries()) {
+        const configuration = value;
+        console.log(configuration.position);
+        bands.push(<div key={index} className="Resistor-band" style={{ left: configuration.position, backgroundColor: configuration.selectedColor.color }}></div>);
+    }
+
     return (
         <div className="Resistor">
             <div className="Resistor-wire"></div>
-            <div className="Resistor-body"></div>
+            <div className="Resistor-body">
+                {bands}
+            </div>
             <div className="Resistor-wire"></div>
         </div>
     );
--- ohm-calculator/src/pages/MainPage/MainPage.js
+++ ohm-calculator/src/pages/MainPage/MainPage.js
@@ -1,13 +1,19 @@
 import './MainPage.css'
 import Resistor from '../../components/Resistor/Resistor'
+import BandColorConfiguration from '../../models/BandColorConfiguration';
 
 function MainPage() {
+    const bandsConfiguration = [
+        new BandColorConfiguration('First band', [{color: '#ff0000', value: 1}], '10px'),
+        new BandColorConfiguration('Second band', [{color: '#0000ff', value: 1}], '30px'),
+    ];
+
     return (
         <div className="MainPage">
             <header>
             <h1>Ohms Calculator</h1>
             </header>
-            <Resistor />
+            <Resistor bandsConfiguration={bandsConfiguration}/>
         </div>
     )
 }
